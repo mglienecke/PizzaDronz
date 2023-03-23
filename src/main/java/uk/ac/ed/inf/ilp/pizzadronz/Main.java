993a99daeb95f7e1acdbd7d8c9c25f5cc26435d5
MICHAEL GLIENECKE <michael@glienecke.de>
2023-03-23 10:19:07 +0000

    Initial version

---
+++ src/main/java/uk/ac/ed/inf/ilp/pizzadronz/Main.java
@@ -0,0 +1,7 @@
+package uk.ac.ed.inf.ilp.pizzadronz;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from the PizzaDronz service!");
+    }
+}
